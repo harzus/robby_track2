19ead54a3d3b6041de70414b1d982d3d6a10b192
Markus Hartinger <mail@markus-hartinger.de>
2016-03-19 20:59:39 +0100

    working velocity commands

--- firmware/rtArduino.cpp
+++ firmware/rtArduino.cpp
@@ -1,6 +1,8 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+#include <roscpp/Logger.h>
 
 #include <Arduino.h>
 
@@ -18,13 +20,87 @@ int M2 = 7; //M2 Direction Control
 
 // start one side of the robot
 void messageCb( const std_msgs::Empty& toggle_msg){
-  analogWrite (E1,255);
-  digitalWrite(M1,LOW);
+//  analogWrite (E1,255);
+//  digitalWrite(M1,LOW);
 }
 
 // subcriber for ping message
 ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
 
+// velocity
+geometry_msgs::Twist twist;
+
+// veloctiy callback
+void velocityCallback(const geometry_msgs::Twist& vel)
+{
+  twist.linear = vel.linear;
+  twist.angular = vel.angular;
+}
+
+// subscriber for velocity
+ros::Subscriber<geometry_msgs::Twist> velocity_sub("robby_track_1/cmd_vel", &velocityCallback);
+
+void setupMotor()
+{
+  int i;
+  for(i=E1;i<=M1;i++)
+  pinMode(i, OUTPUT);
+}
+
+void stop() //Stop
+{
+  digitalWrite(E1,LOW);
+  digitalWrite(E2,LOW);
+}
+
+void forward(char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);
+  analogWrite (E2,b);
+  digitalWrite(M2,LOW);
+}
+
+void reverse (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);
+  analogWrite (E2,b);
+  digitalWrite(M2,HIGH);
+}
+
+void left (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);
+  analogWrite (E2,b);
+  digitalWrite(M2,LOW);
+}
+
+void right (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);
+  analogWrite (E2,b);
+  digitalWrite(M2,HIGH);
+}
+
+void loopMotor()
+{
+    int leftspeed = 255; //255 is maximum speed
+    int rightspeed = 255;
+    if (twist.linear.x > 1.0) {
+        forward (leftspeed,rightspeed);
+    } else if (twist.linear.x < -1.0) {
+        reverse (leftspeed,rightspeed);
+    } else if (twist.angular.z > 1.0) {
+        left (leftspeed,rightspeed);
+    } else if (twist.angular.z < 1.0) {
+        right (leftspeed,rightspeed);
+    } else {
+        stop();
+    }
+}
 
 void setup()
 {
@@ -34,9 +110,9 @@ void setup()
   nh.advertise(chatter);
   // subscribe
   nh.subscribe(sub);
-  int i;
-  for(i=5;i<=8;i++)
-  pinMode(i, OUTPUT);
+  // subscribe
+  nh.subscribe(velocity_sub);
+  setupMotor();
 }
 
 void loop()
@@ -44,64 +120,7 @@ void loop()
   str_msg.data = hello;
   chatter.publish( &str_msg );
   nh.spinOnce();
+  loopMotor();
   delay(1000);
 }
 
-//test
-/*#include <ros.h>
-#include <std_msgs/String.h>
-//#include <tf/transform_broadcaster.h>
-//#include <tf/transform_listener.h>
-//#include <geometry_msgs/Twist.h>
-#include <ros/time.h>
-#include <std_msgs/Empty.h>
-//#include <std_msgs/Float64.h>
-#include <Arduino.h>
-
-ros::NodeHandle nh;
-
-int count;
-//float lin_vel;
-//float ang_vel;
-//ros::WallTime last_command_time;
-
-void messageCb( const std_msgs::Empty& toggle_msg){
-  count++;
-}
-
-
-//void velocityCallback(const geometry_msgs::Twist::ConstPtr& vel)
-//{
-//  last_command_time = ros::WallTime::now();
-//  lin_vel = vel->linear.x;
-//  ang_vel = vel->angular.z;
-//}
-
-ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
-//ros::Subscriber<geometry_msgs::Twist> rtTwist("robby_track_1/cmd_vel", 1);
-
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
-
-char hello1[15] = "hello world 1!";
-char hello2[15] = "hello world 2!";
-
-void setup()
-{
-  nh.initNode();
-  nh.advertise(chatter);
-  nh.subscribe(sub);
-}
-
-void loop()
-{
-  if ((count % 2) == 0) {
-    str_msg.data = hello1;
-  } else {
-    str_msg.data = hello2;
-  }
-  chatter.publish( &str_msg );
-  nh.spinOnce();
-  delay(1000);
-}
-*/
